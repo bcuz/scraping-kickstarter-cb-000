5bd3f3d43e0e01cdff52148cd5e67c5e7596c993
adam n <adam.nawroc@gmail.com>
2018-03-09 16:52:10 +0000

    automatically backed up by learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -9,7 +9,9 @@ def create_project_hash
 
   kickstarter = Nokogiri::HTML(html)
   kickstarter.css('.project.grid_4').each do |pj|
-    projects[pj.css('.bbcard_name strong a').text] = {}
+    projects[pj.css('.bbcard_name strong a').text] = {
+      
+    }
   end
 
   # im bored.
