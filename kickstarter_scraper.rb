5060256760686a958671126f645564c482f90caa
adam n <adam.nawroc@gmail.com>
2018-03-09 17:01:20 +0000

    automatically backed up by learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -11,7 +11,8 @@ def create_project_hash
   kickstarter.css('.project.grid_4').each do |pj|
     projects[pj.css('.bbcard_name strong a').text] = {
       image_link: pj.css('img').attribute('src').text,
-      description: pj.css('bbcard_blurb').text,
+      description: pj.css('.bbcard_blurb').text,
+      # location: pj.css('bbcard_blurb').text,
 
 
 
